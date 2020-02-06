9bf840185a9c67034e73b715bfaeceb5f53f1c5d
Dennis LLopis <dllopis16@gmail.com>
2020-02-06 17:47:28 -0500

    Revamped site to a simpler look without much scrolling

---
+++ assets/scripts/index.js
@@ -0,0 +1,10 @@
+
+/* Credit to: https://codepen.io/shaikmaqsood/pen/XmydxJ */
+function copyToClipboard(element) {
+    
+    var $temp = $("<input>");
+    $("body").append($temp);
+    $temp.val($(element).text()).select();
+    document.execCommand("copy");
+    $temp.remove();
+  }
